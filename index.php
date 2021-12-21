bb49cd624eb49fc98b7ffcd36824673c87a7f0ff
Miftahul Dewanto <90891230+MiftahulDewanto@users.noreply.github.com>
2021-12-21 11:17:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+    
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,6 +44,7 @@
 
         <script src="js/script.js"></script>
     </body>
-    
-</body>
+
 </html>
+
+    
